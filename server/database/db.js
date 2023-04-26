43bbf6e1a6d32626267d02b3fecdad4ac128f813
Bhavesh Garg <developerbhaveshgarg@gmail.com>
2023-04-26 17:34:49 +0530

    Update db.js

--- server/database/db.js
+++ server/database/db.js
@@ -7,7 +7,7 @@ const DBConnection = async () => {
     const USERNAME = process.env.DB_USERNAME;
     const PASSWORD = process.env.DB_PASSWORD;
 
-    const MONGO_URI = `mongodb+srv://bhaveshgarg151:admin123@cluster0.8gz6wtr.mongodb.net/test`;
+    const MONGO_URI = process.env.MONGODB_URL;
     try {
         await mongoose.connect(MONGO_URI, { useNewUrlParser: true });
         console.log('Database connected successfully');
