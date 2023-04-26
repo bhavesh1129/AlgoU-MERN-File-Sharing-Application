f32501c55b79e3fb31636ab86e02fb46dbb434a6
Bhavesh Garg <developerbhaveshgarg@gmail.com>
2023-04-26 17:29:53 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,53 @@
+import { useState, useEffect, useRef } from 'react';
+import './App.css';
+import { uploadFile } from './service/api';
+
+function App() {
+  const [file, setFile] = useState('');
+  const [result, setResult] = useState('');
+
+  const fileInputRef = useRef();
+
+  const url = 'https://i.pinimg.com/originals/16/46/24/1646243661201a0892cc4b1a64fcbacf.jpg';
+
+  useEffect(() => {
+    const getImage = async () => {
+      if (file) {
+        const data = new FormData();
+        data.append("name", file.name);
+        data.append("file", file);
+
+        const response = await uploadFile(data);
+        setResult(response.path);
+      }
+    }
+    getImage();
+  }, [file])
+
+  const onUploadClick = () => {
+    fileInputRef.current.click();
+  }
+
+  return (
+    <div className='main-wrapper' style={{ backgroundImage: `url('https://images.pexels.com/photos/23547/pexels-photo.jpg')` }}>
+      <div className='container'>
+        <div className='wrapper'>
+          <h1>AlgoU File Sharing!</h1>
+          <p>Upload and share the download link🔗.</p>
+
+          <button onClick={() => onUploadClick()}>Upload</button>
+          <input
+            type="file"
+            ref={fileInputRef}
+            style={{ display: "none" }}
+            onChange={(e) => setFile(e.target.files[0])}
+          />
+
+          <a href={result} target='_blank'>{result}</a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ server/database/db.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const DBConnection = async () => {
+    const USERNAME = process.env.DB_USERNAME;
+    const PASSWORD = process.env.DB_PASSWORD;
+
+    const MONGO_URI = `mongodb+srv://bhaveshgarg151:admin123@cluster0.8gz6wtr.mongodb.net/test`;
+    try {
+        await mongoose.connect(MONGO_URI, { useNewUrlParser: true });
+        console.log('Database connected successfully');
+    } catch (error) {
+        console.log('Error while connecting with the database ', error.message);
+    }
+}
+
+export default DBConnection;
